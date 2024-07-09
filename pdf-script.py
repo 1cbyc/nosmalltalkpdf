7c0881d23fa231d30252df5b517da1055d2f1f16
1cbyc <isaacnsisong@gmail.com>
2024-07-09 13:48:55 +0100

    detail

---
+++ pdf-script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+##
+## you can create different types of malicious PDF files. you can use it for penetration testing and/or red-teaming, and the likes...
+##
+## Usage ./pdf-script.py burp-collaborator-url
+##
+## most times, output will be written as: test1.pdf, test2.pdf, test3.pdf and test4.pdf
+##
